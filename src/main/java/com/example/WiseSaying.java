69c33fd9b4b9315ce3901ce92601b59cd1b25dae
jongmin <chungjm0711@gmail.com>
2022-07-14 15:29:21 +0900

    feat : string -> json, map 을 통한 생성자 처리

--- src/main/java/com/example/WiseSaying.java
+++ src/main/java/com/example/WiseSaying.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.Map;
+
 public class WiseSaying {
     private int id;
     private String content;
@@ -11,6 +13,12 @@ public class WiseSaying {
         this.author = author;
     }
 
+    public WiseSaying(Map<String, Object> map) {
+        this.id = (int) map.get("id");
+        this.content = (String) map.get("content");
+        this.author = (String) map.get("author");
+    }
+
     @Override
     public String toString() {
         return "WiseSaying{" +
@@ -18,6 +26,19 @@ public class WiseSaying {
                 ", content='" + content + '\'' +
                 ", author='" + author + '\'' +
                 '}';
+    }
+
+    public String toJson() {
+        return """
+                {
+                    "id": %d,
+                    "content": "%s",
+                    "author": "%s"
+                }
+                """
+                .stripIndent()
+                .formatted(id, content, author)
+                .trim();
     }
 
     public void setContent(String content) {
@@ -39,4 +60,27 @@ public class WiseSaying {
     public String getAuthor() {
         return author;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof WiseSaying)) return false;
+
+        WiseSaying other = (WiseSaying) o;
+
+        if (this.id != other.id) return false;
+        if (!this.content.equals(other.content)) return false;
+        if (!this.author.equals(other.author)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (content != null ? content.hashCode() : 0);
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        return result;
+    }
 }
+
