68c06b3eacac589295db53bb0ca529faea5791fd
jongmin <chungjm0711@gmail.com>
2022-07-14 21:54:14 +0900

    test: file 을 통한 입출력 확인

--- src/test/java/com/example/FileDBTest.java
+++ src/test/java/com/example/FileDBTest.java
@@ -3,7 +3,10 @@ package com.example;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,6 +17,64 @@ public class FileDBTest {
         Util.File.deleteDir("test_data");
         Util.File.mkdir("test_data");
     }
+
+    @Test
+    void 특정_폴더에_들어있는_JSON_파일들만_모아서_객체화_시킨_후_리스트에_담기() {
+        WiseSaying wiseSaying1 = new WiseSaying(1, "내 사전에 불가능은 없다.", "나폴레옹");
+        WiseSaying wiseSaying2 = new WiseSaying(2, "나의 죽음을 적들에게 알리지 마라.", "이순신");
+
+        Util.File.saveToFile("test_data/1.json", wiseSaying1.toJson());
+        Util.File.saveToFile("test_data/2.json", wiseSaying2.toJson());
+        Util.File.saveNumberToFile("test_data/last_id.txt", 2);
+
+        List<String> fileNames =
+                Util.File.getFileNamesFromDir("test_data")
+                        .stream() // 리스트를 스트림으로 변환
+                        .filter(fileName -> fileName.endsWith(".json")) // 파일명이 .json 으로 끝나지 않는 것들은 모두 버림
+                        .collect(Collectors.toList()); // 스트림을 다시 리스트로 변환
+
+        Function<Map, WiseSaying> f = WiseSaying::new;
+
+        List<WiseSaying> wiseSayings = fileNames
+                .stream()
+                .map(fileName -> Util.File.readFromFile("test_data/" + fileName))
+                .map(Util.Json::jsonToMap)
+                .map(f)
+                .collect(Collectors.toList());
+
+//        List<WiseSaying> wiseSayings = new ArrayList<>();
+//        for (String fileName : fileNames) {
+//            String rs = Util.readFromFile("test_data/" + fileName);
+//            Map<String, Object> map = Util.jsonToMap(rs);
+//            WiseSaying wiseSaying = new WiseSaying(map);
+//            wiseSayings.add(wiseSaying);
+//        }
+
+        assertEquals(wiseSaying1, wiseSayings.get(0));
+        assertEquals(wiseSaying2, wiseSayings.get(1));
+    }
+
+    @Test
+    void 특정_폴더에_존재하는_모든_파일의_이름들을_가져온다() {
+        Util.File.saveNumberToFile("test_data/1.txt", 1);
+        Util.File.saveNumberToFile("test_data/2.txt", 2);
+        Util.File.saveNumberToFile("test_data/3.txt", 3);
+
+        List<String> fileNames = Util.File.getFileNamesFromDir("test_data");
+
+        assertEquals("1.txt", fileNames.get(0));
+        assertEquals("2.txt", fileNames.get(1));
+        assertEquals("3.txt", fileNames.get(2));
+    }
+
+    @Test
+    void 파일에_숫자_저장() {
+        Util.File.saveNumberToFile("test_data/last_id.txt", 100);
+        int rs = Util.File.readNumberFromFile("test_data/last_id.txt", 0);
+
+        assertEquals(100, rs);
+    }
+
     @Test
     void 파일에_있는_JSON을_객체로_변환() {
         WiseSaying wiseSaying = new WiseSaying(1, "내 사전에 불가능은 없다.", "나폴레옹");
