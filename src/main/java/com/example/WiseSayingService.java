fba52821a7fae4cc765083d96edc99f2c212e095
jongmin <chungjm0711@gmail.com>
2022-07-14 21:53:04 +0900

    refactor: 메서드 명명 직관적으로 refactor + 빌드 service 추가

--- src/main/java/com/example/WiseSayingService.java
+++ src/main/java/com/example/WiseSayingService.java
@@ -1,24 +1,25 @@
 package com.example;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class WiseSayingService {
     private WiseSayingRepository wiseSayingRepository;
 
     public WiseSayingService() {
-        wiseSayingRepository = new WiseSayingRepository();
+        wiseSayingRepository = new WiseSayingRepository(App.getBaseDir());
     }
 
     public WiseSaying write(String content, String author) {
-        return wiseSayingRepository.add(content, author);
+        return wiseSayingRepository.save(content, author);
     }
 
     public boolean modify(int id, String content, String author) {
-        return wiseSayingRepository.modify(id, content, author);
+        return wiseSayingRepository.update(id, content, author);
     }
 
     public boolean remove(int id) {
-        return wiseSayingRepository.remove(id);
+        return wiseSayingRepository.removeById(id);
     }
 
     public WiseSaying findById(int id) {
@@ -27,5 +28,16 @@ public class WiseSayingService {
 
     public List<WiseSaying> findAll() {
         return wiseSayingRepository.findAll();
+    }
+
+    public void dumpToJson() {
+        List<WiseSaying> wiseSayings = wiseSayingRepository.findAll();
+
+        String json = "[\n\t" + wiseSayings
+                .stream()
+                .map(WiseSaying::toJson)
+                .collect(Collectors.joining(", ")) + "\n]";
+
+        Util.File.saveToFile("%s/data.json".formatted(App.getBaseDir()), json);
     }
 }
