5ed35bb0034af2c6f7fa5638c5e8b73f7ad383bc
jongmin <chungjm0711@gmail.com>
2022-07-14 21:52:25 +0900

    feat: build 메서드 추가 및 메서드 파라미터 제거

--- src/main/java/com/example/WiseSayingController.java
+++ src/main/java/com/example/WiseSayingController.java
@@ -12,7 +12,7 @@ public class WiseSayingController {
         wiseSayingService = new WiseSayingService();
     }
 
-    public void write(Rq rq) {
+    public void write() {
         System.out.print("명언 : ");
         String content = sc.nextLine();
         System.out.print("작가 : ");
@@ -23,7 +23,7 @@ public class WiseSayingController {
         System.out.printf("%d번 명언이 등록되었습니다.%n", wiseSaying.getId());
     }
 
-    public void list(Rq rq) {
+    public void list() {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------------");
 
@@ -82,5 +82,9 @@ public class WiseSayingController {
         wiseSayingService.remove(wiseSaying.getId());
 
         System.out.printf("%d번 명언이 삭제되었습니다.\n", id);
+    }
+
+    public void build() {
+        wiseSayingService.dumpToJson();
     }
 }
