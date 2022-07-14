384ecd6d8c256d8a3aee604c6a106f9dd6dc5e1f
jongmin <chungjm0711@gmail.com>
2022-07-14 16:48:15 +0900

    feat: main에서 Scanner 주입

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,70 +1,39 @@
 package com.example;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-/**
- * == 명언 SSG ==
- * 명령) 등록
- * 명언 : 현재를 사랑하라.
- * 작가 : 작자미상
- * 1번 명언이 등록되었습니다.
- * 명령) 등록
- * 명언 : 과거에 집착하지 마라.
- * 작가 : 작자미상
- * 2번 명언이 등록되었습니다.
- * 명령) 목록
- * 번호 / 작가 / 명언
- * ----------------------
- * 2 / 작자미상 / 과거에 집착하지 마라.
- * 1 / 작자미상 / 현재를 사랑하라.
- * 명령) 삭제?id=1
- * 1번 명언이 삭제되었습니다.
- * 명령) 삭제?id=1
- * 1번 명언은 존재하지 않습니다.
- * 명령) 수정?id=2
- * 2번 명언을 수정합니다.
- * 기존 명언 : 과거에 집착하지 마라.
- * 새 명언 : 미래와 과거에 집착하지 마라.
- * 2번 명언이 수정되었습니다.
- * 명령) 목록
- * 번호 / 작가 / 명언
- * ----------------------
- * 2 / 작자미상 / 미래와 과거에 집착하지 마라.
- */
 public class App {
 
     private final Scanner sc;
 
-    public App() {
-        this.sc = new Scanner(System.in);
+    public App(Scanner sc) {
+        this.sc = sc;
     }
 
     public void run() {
 
         System.out.println("== 명언 SSG ==");
 
-        Controller controller = new Controller(sc);
+        WiseSayingController controller = new WiseSayingController(sc);
 
         outer:
         while (true) {
             System.out.print("명령) ");
             String cmd = sc.nextLine().trim();
 
-            Request request = new Request(cmd);
-            switch (request.getPath()) {
+            Rq rq = new Rq(cmd);
+            switch (rq.getPath()) {
                 case "등록":
-                    controller.write(request);
+                    controller.write(rq);
                     break;
                 case "목록":
-                    controller.list(request);
+                    controller.list(rq);
                     break;
                 case "수정":
-                    controller.modify(request);
+                    controller.modify(rq);
                     break;
                 case "삭제":
-                    controller.remove(request);
+                    controller.remove(rq);
                     break;
                 case "종료":
                     break outer;
