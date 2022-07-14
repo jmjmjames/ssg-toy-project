40200452082f25ff4bc8dcb787f888e92cf0e42c
jongmin <chungjm0711@gmail.com>
2022-07-14 21:53:33 +0900

    feat: Map -> File 입출력으로 변경

--- src/main/java/com/example/WiseSayingRepository.java
+++ src/main/java/com/example/WiseSayingRepository.java
@@ -1,61 +1,88 @@
 package com.example;
 
-import java.util.ArrayList;
+import java.io.File;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class WiseSayingRepository {
-    private List<WiseSaying> wiseSayings;
-    private int wiseSayingsLastId;
 
-    public WiseSayingRepository() {
-        this.wiseSayings = new ArrayList<>();
-        wiseSayingsLastId = 0;
+    private String baseDir;
+
+    public WiseSayingRepository(String baseDir) {
+        this.baseDir = baseDir;
     }
 
-    public WiseSaying add(String content, String author) {
-        int id = ++wiseSayingsLastId;
+
+    public void save(WiseSaying wiseSaying) {
+        Util.File.mkdir("%s/wise_saying".formatted(baseDir));
+        String body = wiseSaying.toJson();
+        Util.File.saveToFile("%s/wise_saying/%d.json".formatted(baseDir, wiseSaying.getId()), body);
+    }
+
+    public WiseSaying save(String content, String author) {
+        int id = getLastId() + 1;
+
         WiseSaying wiseSaying = new WiseSaying(id, content, author);
-        wiseSayings.add(wiseSaying);
+        save(wiseSaying);
+
+        saveLastId(id);
 
         return wiseSaying;
     }
 
+    public boolean update(int id, String content, String author) {
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+
+        save(wiseSaying);
+        return true;
+    }
+
+    private void saveLastId(int id) {
+        Util.File.saveToFile("%s/wise_saying/last_id.txt".formatted(baseDir), id + "");
+    }
+
     public List<WiseSaying> findAll() {
-        return wiseSayings;
-    }
+        String path = "%s/wise_saying".formatted(baseDir);
+        List<String> fileNames = Util.File.getFileNamesFromDir(path);
 
-    public boolean remove(int id) {
-        WiseSaying wiseSaying = findById(id);
+        List<Integer> fileIds = fileNames
+                .stream()
+                .filter(fileName -> !fileName.equals("last_id.txt"))
+                .map(fileName -> fileName.replace(".json", ""))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
 
-        if (wiseSaying == null) {
-            return false;
-        }
-
-        wiseSayings.remove(wiseSaying);
-
-        return true;
-    }
-
-    public boolean modify(int id, String content, String author) {
-        WiseSaying wiseSaying = findById(id);
-
-        if (wiseSaying == null) {
-            return false;
-        }
-
-        wiseSaying.setContent(content);
-        wiseSaying.setAuthor(author);
-
-        return true;
+        return fileIds
+                .stream()
+                .map(this::findById)
+                .collect(Collectors.toList());
     }
 
     public WiseSaying findById(int id) {
-        for (WiseSaying wiseSaying : wiseSayings) {
-            if (wiseSaying.getId() == id) {
-                return wiseSaying;
-            }
-        }
-        return null;
+        String path = "%s/wise_saying/%d.json".formatted(baseDir, id);
+
+        if (!(new File(path).exists())) return null;
+
+        Map<String, Object> map = Util.Json.jsonToMap(path);
+
+        if (map == null) return null;
+
+        return new WiseSaying(map);
     }
+
+    public int getLastId() {
+        String lastId = Util.File.readFromFile("%s/wise_saying/last_id.txt".formatted(baseDir));
+
+        if (lastId.isEmpty()) return 0;
+        return Integer.parseInt(lastId);
+    }
+
+    public boolean removeById(int id) {
+        String path = "%s/wise_saying/%d.json".formatted(baseDir, id);
+        return new File(path).delete();
+    }
+
 }
 
